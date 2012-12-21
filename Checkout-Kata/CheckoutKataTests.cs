d8b36cad0c5ecd25ec356984923e7fbd28af5641
Chris Watson <chris.watson@7digital.com>
2012-12-21 10:43:30 +0000

    Add scan a

---
+++ Checkout-Kata/CheckoutKataTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Checkout_Kata
+{
+	[TestFixture]
+    public class CheckoutKataTests
+    {
+		[Test]
+		public void Scan_should_return_50_for_a()
+		{
+			var actual = Scan('a');
+			var expected = 50;
+			Assert.That(actual, Is.EqualTo(expected));
+		}
+
+		private int Scan(char item)
+		{
+			return 50;
+		}
+    }
+}
