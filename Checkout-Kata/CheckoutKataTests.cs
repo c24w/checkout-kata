a5a7d7a4be5a80c99c92cfadf00de019e3a1dc24
Chris Watson <chris.watson@7digital.com>
2012-12-21 10:48:46 +0000

    Add scan c

--- Checkout-Kata/CheckoutKataTests.cs
+++ Checkout-Kata/CheckoutKataTests.cs
@@ -8,16 +8,22 @@ namespace Checkout_Kata
 		[Test]
 		[TestCase('a', 50)]
 		[TestCase('b', 30)]
+		[TestCase('c', 20)]
 		public void Scan_should_return_50_for_a(char item, int expected)
 		{
-			var actual = Scan(item);
+			var actual = new Scanner().Scan(item);
 			Assert.That(actual, Is.EqualTo(expected));
 		}
+	}
 
-		private int Scan(char item)
+	class Scanner
+	{
+		public int Scan(char item)
 		{
 			if (item == 'b')
 				return 30;
+			if (item == 'c')
+				return 20;
 			return 50;
 		}
 	}
