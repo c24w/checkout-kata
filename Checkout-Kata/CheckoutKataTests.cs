a32d15bbdd1f35de8a7d18c3afa2c5215f9bee98
Chris Watson <chris.watson@7digital.com>
2013-01-02 11:11:10 +0000

    Refactor tests

--- Checkout-Kata/CheckoutKataTests.cs
+++ Checkout-Kata/CheckoutKataTests.cs
@@ -6,7 +6,7 @@ namespace Checkout_Kata
 	[TestFixture]
 	public class CheckoutKataTests
 	{
-		private readonly IEnumerable<ItemPrice> _prices = new []
+		private readonly IEnumerable<ItemPrice> _prices = new[]
 		{
 			new ItemPrice{ItemCode = 'a', Price = 50},
 			new ItemPrice{ItemCode = 'b', Price = 30},
@@ -49,40 +49,31 @@ namespace Checkout_Kata
 		[TestCase("b", 30)]
 		[TestCase("c", 20)]
 		[TestCase("d", 15)]
-		public void Scan_returns_expected_price_for_item(string items, int expected)
+		public void Scan_one_item_returns_expected_total(string items, int expected)
 		{
 			var actual = new Cashier(_prices, _discounters).ScanBasket(items);
 			Assert.That(actual, Is.EqualTo(expected));
 		}
 
-		[Test]
 		[TestCase("aa", 100)]
 		[TestCase("ab", 80)]
 		[TestCase("abc", 100)]
 		[TestCase("aabc", 150)]
-		public void Multiple_items_ineligible_for_discount_returns_expected_total(string items, int expected)
+		public void Scan_multiple_items_ineligible_for_discount_returns_expected_total(string items, int expected)
 		{
 			var actual = new Cashier(_prices, _discounters).ScanBasket(items);
 			Assert.That(actual, Is.EqualTo(expected));
 		}
 
-		[Test]
 		[TestCase("aaa", 130)]
-		[TestCase("aaaaaa", 260)]
-		[TestCase("bb", 45)]
-		[TestCase("bbbb", 90)]
-		public void Multiple_of_the_same_item_eligible_for_discount_returns_expected_total(string items, int expected)
-		{
-			var actual = new Cashier(_prices, _discounters).ScanBasket(items);
-			Assert.That(actual, Is.EqualTo(expected));
-		}
-
-		[Test]
 		[TestCase("aaab", 160)]
-		[TestCase("abb", 95)]
+		[TestCase("aaaaaa", 260)]
 		[TestCase("aaaaaabb", 305)]
+		[TestCase("bb", 45)]
+		[TestCase("abb", 95)]
+		[TestCase("bbbb", 90)]
 		[TestCase("bbbbacd", 175)]
-		public void Multiple_items_eligible_for_discount_returns_expected_total(string items, int expected)
+		public void Scan_multiple_items_eligible_for_discount_returns_expected_total(string items, int expected)
 		{
 			var actual = new Cashier(_prices, _discounters).ScanBasket(items);
 			Assert.That(actual, Is.EqualTo(expected));
